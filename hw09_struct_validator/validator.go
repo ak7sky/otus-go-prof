805b17410308d2514b96b5a5f0ec39adfe175a48
k7sky <alex.k7sky@gmail.com>
2023-09-02 19:32:51 +0300

    hw09 struct validator

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,338 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+const (
+	validateTag = "validate"
+
+	strType    = "string"
+	strSlcType = "[]string"
+	intType    = "int"
+	intSlcType = "[]int"
+	structType = "struct"
+
+	lenRestr    = "len"
+	regexpRestr = "regexp"
+	inRestr     = "in"
+	minRestr    = "min"
+	maxRestr    = "max"
+	nested      = "nested"
+)
+
+var (
+	// app errors.
+	errNotStruct         = errors.New("passed arg must be a go struct type")
+	errNotNested         = errors.New("supported validate tag for nested struct is 'nested'")
+	errEmptyVldTag       = errors.New("validate tag value must not be empty")
+	errFieldType         = errors.New("unsupported field type; supported: string, []string, int, []int")
+	errRestrType         = errors.New("unknown restriction for field type")
+	errNotIntLenRestr    = errors.New("value of 'len' restriction must be int")
+	errNotIntMinRestr    = errors.New("value of 'min' restriction must be int")
+	errNotIntMaxRestr    = errors.New("value of 'max' restriction must be int")
+	errNotIntInRestr4int = errors.New("all values of 'in' restriction must be integers for int type")
+
+	// vld error.
+	errValidation = errors.New("violations")
+)
+
+type fieldValidator func(name string, value reflect.Value, restrs []string) error
+
+var fieldValidators map[string]fieldValidator
+
+// to avoid initialization loop fieldValidators -> structValidator -> validateField -> fieldValidators.
+func init() {
+	fieldValidators = map[string]fieldValidator{
+		strType:    strValidator,
+		strSlcType: strSlcValidator,
+		intType:    intValidator,
+		intSlcType: intSlcValidator,
+		structType: structValidator,
+	}
+}
+
+type strRestrMatcher func(val string, restrVal string) (bool, error)
+
+var strRestrMatchers = map[string]strRestrMatcher{
+	lenRestr:    strLenRestrMatcher,
+	regexpRestr: strRegexpRestrMatcher,
+	inRestr:     strInRestrMatcher,
+}
+
+type intRestrMatcher func(val int, restrVal string) (bool, error)
+
+var intRestrMatchers = map[string]intRestrMatcher{
+	minRestr: intMinRestrMatcher,
+	maxRestr: intMaxRestrMatcher,
+	inRestr:  intInRestrMatcher,
+}
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
 
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("field: '%s'; %s", v.Field, v.Err)
+}
+
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	builder := strings.Builder{}
+	builder.WriteString("validation errors:\n")
+
+	for _, ve := range v {
+		builder.WriteString("\t")
+		builder.WriteString(ve.Error())
+		builder.WriteString("\n")
+	}
+
+	return builder.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	val := reflect.ValueOf(v)
+
+	if val.Kind() != reflect.Struct {
+		return errNotStruct
+	}
+
+	return structValidator("", val, []string{})
+}
+
+func validateField(fieldMeta reflect.StructField, fieldVal reflect.Value) error {
+	validateTagVal, validationRequired := fieldMeta.Tag.Lookup(validateTag)
+	if !validationRequired {
+		return nil
+	}
+	if validateTagVal == "" {
+		return fmt.Errorf("field '%s': %w", fieldMeta.Name, errEmptyVldTag)
+	}
+
+	fieldType := underlyingTypeOf(fieldVal)
+	fv, fieldTypeSupported := fieldValidators[fieldType]
+	if !fieldTypeSupported {
+		return fmt.Errorf("field '%s', type %s: %w", fieldMeta.Name, fieldType, errFieldType)
+	}
+
+	fieldRestrs := strings.Split(validateTagVal, "|")
+	return fv(fieldMeta.Name, fieldVal, fieldRestrs)
+}
+
+func underlyingTypeOf(value reflect.Value) string {
+	kind := value.Kind()
+	if kind == reflect.Slice {
+		if value.CanConvert(reflect.TypeOf([]string{})) {
+			return strSlcType
+		}
+		if value.CanConvert(reflect.TypeOf([]int{})) {
+			return intSlcType
+		}
+	}
+	return kind.String()
+}
+
+func structValidator(name string, value reflect.Value, restrs []string) error {
+	if name != "" && (len(restrs) > 1 || restrs[0] != nested) {
+		return fmt.Errorf("field '%s': %w", name, errNotNested)
+	}
+
+	var verrors ValidationErrors
+
+	for _, fieldMeta := range reflect.VisibleFields(value.Type()) {
+		fieldVal := value.FieldByName(fieldMeta.Name)
+		err := validateField(fieldMeta, fieldVal)
+		if err == nil {
+			continue
+		}
+		vldErr := &ValidationError{}
+		if errors.As(err, vldErr) {
+			verrors = append(verrors, *vldErr)
+			continue
+		}
+		return err
+	}
+
+	if verrors != nil {
+		if name != "" {
+			return ValidationError{Field: name, Err: verrors}
+		}
+		return verrors
+	}
+
 	return nil
+}
+
+func strValidator(name string, value reflect.Value, restrs []string) error {
+	val := value.Convert(reflect.TypeOf("")).Interface().(string)
+	var violatedRestrs []string
+	for _, restr := range restrs {
+		restrTypeAndVal := strings.Split(restr, ":")
+		restrType, restrVal := restrTypeAndVal[0], restrTypeAndVal[1]
+		restrMatcher, restrTypeSupported := strRestrMatchers[restrType]
+		if !restrTypeSupported {
+			return fmt.Errorf("field '%s', type %s, restr type '%s': %w", name, strType, restrType, errRestrType)
+		}
+		isMatched, err := restrMatcher(val, restrVal)
+		if err != nil {
+			return fmt.Errorf("field '%s': %w", name, err)
+		}
+		if !isMatched {
+			violatedRestrs = append(violatedRestrs, restr)
+		}
+	}
+	if violatedRestrs != nil {
+		return ValidationError{
+			Field: name,
+			Err:   fmt.Errorf("%w: val '%s', %v", errValidation, val, violatedRestrs),
+		}
+	}
+
+	return nil
+}
+
+func strSlcValidator(name string, value reflect.Value, restrs []string) error {
+	vals := value.Convert(reflect.TypeOf([]string{})).Interface().([]string)
+	var elVldErrors []error
+
+	for idx, val := range vals {
+		err := strValidator(name, reflect.ValueOf(val), restrs)
+		if err == nil {
+			continue
+		}
+
+		vldErr := &ValidationError{}
+		if errors.As(err, vldErr) {
+			elVldErrors = append(elVldErrors, fmt.Errorf("idx %d: %w", idx, vldErr.Err))
+			continue
+		}
+		return err
+	}
+
+	if elVldErrors != nil {
+		return ValidationError{
+			Field: name,
+			Err:   fmt.Errorf("%w: %v", errValidation, elVldErrors),
+		}
+	}
+
+	return nil
+}
+
+func intValidator(name string, value reflect.Value, restrs []string) error {
+	val := value.Convert(reflect.TypeOf(0)).Interface().(int)
+	var violatedRestrs []string
+	for _, restr := range restrs {
+		restrTypeAndVal := strings.Split(restr, ":")
+		restrType, restrVal := restrTypeAndVal[0], restrTypeAndVal[1]
+		restrMatcher, restrTypeSupported := intRestrMatchers[restrType]
+		if !restrTypeSupported {
+			return fmt.Errorf("field '%s', type %s, restr type '%s': %w", name, intType, restrType, errRestrType)
+		}
+		isMatched, err := restrMatcher(val, restrVal)
+		if err != nil {
+			return fmt.Errorf("field '%s': %w", name, err)
+		}
+		if !isMatched {
+			violatedRestrs = append(violatedRestrs, restr)
+		}
+	}
+	if violatedRestrs != nil {
+		return ValidationError{
+			Field: name,
+			Err:   fmt.Errorf("%w: val '%d', %v", errValidation, val, violatedRestrs),
+		}
+	}
+
+	return nil
+}
+
+func intSlcValidator(name string, value reflect.Value, restrs []string) error {
+	vals := value.Convert(reflect.TypeOf([]int{})).Interface().([]int)
+	var elVldErrors []error
+
+	for idx, val := range vals {
+		err := intValidator(name, reflect.ValueOf(val), restrs)
+		if err == nil {
+			continue
+		}
+		vldErr := &ValidationError{}
+		if errors.As(err, &ValidationError{}) {
+			elVldErrors = append(elVldErrors, fmt.Errorf("idx %d: %w", idx, vldErr.Err))
+			continue
+		}
+		return err
+	}
+
+	if elVldErrors != nil {
+		return ValidationError{
+			Field: name,
+			Err:   fmt.Errorf("%w: %v", errValidation, elVldErrors),
+		}
+	}
+
+	return nil
+}
+
+func strLenRestrMatcher(val string, lenRestrVal string) (bool, error) {
+	expLen, err := strconv.Atoi(lenRestrVal)
+	if err != nil {
+		return false, errNotIntLenRestr
+	}
+	return utf8.RuneCountInString(val) == expLen, nil
+}
+
+func strRegexpRestrMatcher(val string, regexpRestrVal string) (bool, error) {
+	isMatched, err := regexp.MatchString(regexpRestrVal, val)
+	if err != nil {
+		return false, fmt.Errorf("err on '%s' regexp restriction matching: %w", regexpRestrVal, err)
+	}
+	return isMatched, nil
+}
+
+func strInRestrMatcher(val string, inRestrVal string) (bool, error) {
+	validVals := strings.Split(inRestrVal, ",")
+	for _, vval := range validVals {
+		if vval == val {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func intMinRestrMatcher(val int, minRestrVal string) (bool, error) {
+	min, err := strconv.Atoi(minRestrVal)
+	if err != nil {
+		return false, errNotIntMinRestr
+	}
+	return val >= min, nil
+}
+
+func intMaxRestrMatcher(val int, maxRestrVal string) (bool, error) {
+	max, err := strconv.Atoi(maxRestrVal)
+	if err != nil {
+		return false, errNotIntMaxRestr
+	}
+	return val <= max, nil
+}
+
+func intInRestrMatcher(val int, inRestrVal string) (bool, error) {
+	validVals := strings.Split(inRestrVal, ",")
+	isMatched := false
+	for _, vval := range validVals {
+		i, err := strconv.Atoi(vval)
+		if err != nil {
+			return false, errNotIntInRestr4int
+		}
+		isMatched = isMatched || i == val
+	}
+	return isMatched, nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,59 +2,304 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-type UserRole string
-
-// Test the function on different structures and other types.
 type (
+	UserRole string
+
+	UserPhysParams struct {
+		Age    int `validate:"min:18|max:50"`
+		Height int `validate:"min:140|max:240"`
+		Weight int `validate:"min:45|max:150"`
+	}
+
 	User struct {
-		ID     string `json:"id" validate:"len:36"`
-		Name   string
-		Age    int      `validate:"min:18|max:50"`
-		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole `validate:"in:admin,stuff"`
-		Phones []string `validate:"len:11"`
-		meta   json.RawMessage
-	}
-
-	App struct {
-		Version string `validate:"len:5"`
-	}
-
-	Token struct {
-		Header    []byte
-		Payload   []byte
-		Signature []byte
-	}
-
-	Response struct {
-		Code int    `validate:"in:200,404,500"`
-		Body string `json:"omitempty"`
+		PhysParams UserPhysParams `validate:"nested"`
+		ID         string         `json:"id" validate:"len:36"`
+		Name       string
+		Experience int             `validate:"min:2|max:50"`
+		Email      string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role       UserRole        `validate:"in:admin,stuff"`
+		Phones     []string        `validate:"len:11"`
+		meta       json.RawMessage //nolint:unused
 	}
 )
 
-func TestValidate(t *testing.T) {
-	tests := []struct {
+func TestValidateReturnsAppErr(t *testing.T) {
+	testCases := []struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}{
+		//--------------------- Application error cases ---------------------
+		{
+			name:        "app-err:not struct",
+			in:          map[int]int{},
+			expectedErr: errNotStruct,
+		},
+		{
+			name: "app-err:empty validate tag",
+			in: struct {
+				Pswd string `validate:""`
+			}{},
+			expectedErr: fmt.Errorf("field 'Pswd': %w", errEmptyVldTag),
+		},
+		{
+			name: "app-err:unsupported type",
+			in: struct {
+				BooksByAuthor map[string][]string `validate:"len:10000"`
+			}{},
+			expectedErr: fmt.Errorf("field 'BooksByAuthor', type map: %w", errFieldType),
+		},
+		{
+			name: "app-err:not nested on struct",
+			in: struct {
+				Nested struct{} `validate:"nest"`
+			}{},
+			expectedErr: fmt.Errorf("field 'Nested': %w", errNotNested),
+		},
+		{
+			name: "app-err:unknown restr for int",
+			in: struct {
+				Shoes int `validate:"even:true"`
+			}{},
+			expectedErr: fmt.Errorf("field 'Shoes', type int, restr type 'even': %w", errRestrType),
+		},
+		{
+			name: "app-err:unknown restr for str",
+			in: struct {
+				Palindrome string `validate:"palindrome:true"`
+			}{},
+			expectedErr: fmt.Errorf("field 'Palindrome', type string, restr type 'palindrome': %w", errRestrType),
+		},
+		{
+			name: "app-err:len restr value",
+			in: struct {
+				Name string `validate:"len:any"`
+			}{},
+			expectedErr: fmt.Errorf("field 'Name': %w", errNotIntLenRestr),
+		},
+		{
+			name: "app-err:min restr value",
+			in: struct {
+				Num int `validate:"min:any"`
+			}{},
+			expectedErr: fmt.Errorf("field 'Num': %w", errNotIntMinRestr),
+		},
+		{
+			name: "app-err:max restr value",
+			in: struct {
+				Num int `validate:"max:any"`
+			}{},
+			expectedErr: fmt.Errorf("field 'Num': %w", errNotIntMaxRestr),
+		},
+		{
+			name: "app-err:in restr on int",
+			in: struct {
+				Num int `validate:"in:10,100,thousand"`
+			}{},
+			expectedErr: fmt.Errorf("field 'Num': %w", errNotIntInRestr4int),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+			err := Validate(tc.in)
+			fmt.Println(err)
+			require.Equal(t, tc.expectedErr, err)
+		})
+	}
+}
+
+func TestValidateReturnsSingleElValidationErrs(t *testing.T) {
+	testCases := []struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}{
+		//--------------------- Validation error cases ---------------------
+		{
+			name: "vld-err:nil",
+			in: struct {
+				ID string `validate:"len:36"`
+			}{
+				ID: "d56e42f5-1aa8-4a54-96d9-77be14e725be",
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "vld-err:len",
+			in: struct {
+				ID string `validate:"len:36"`
+			}{
+				ID: "d56e42f5",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "ID",
+					Err:   fmt.Errorf("%w: val 'd56e42f5', %v", errValidation, []string{"len:36"}),
+				},
+			},
+		},
+		{
+			name: "vld-err:min",
+			in: struct {
+				Experience int `validate:"min:2|max:50"`
+			}{
+				Experience: 1,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Experience",
+					Err:   fmt.Errorf("%w: val '1', %v", errValidation, []string{"min:2"}),
+				},
+			},
+		},
+		{
+			name: "vld-err:max",
+			in: struct {
+				Experience int `validate:"min:2|max:50"`
+			}{
+				Experience: 999,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Experience",
+					Err:   fmt.Errorf("%w: val '999', %v", errValidation, []string{"max:50"}),
+				},
+			},
+		},
+		{
+			name: "vld-err:regexp",
+			in: struct {
+				Email string `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+			}{
+				Email: "any@@any.com",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Email",
+					Err:   fmt.Errorf("%w: val 'any@@any.com', %v", errValidation, []string{"regexp:^\\w+@\\w+\\.\\w+$"}),
+				},
+			},
+		},
+		{
+			name: "vld-err:in",
+			in: struct {
+				Role string `validate:"in:admin,stuff"`
+			}{
+				Role: "moder",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Role",
+					Err:   fmt.Errorf("%w: val 'moder', %v", errValidation, []string{"in:admin,stuff"}),
+				},
+			},
+		},
+		{
+			name: "vld-err:len on []string",
+			in: struct {
+				Phones []string `validate:"len:11"`
+			}{
+				Phones: []string{"89123456789", "898765432109"}, // 2nd num is invalid
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Phones",
+					Err:   fmt.Errorf("%w: %v", errValidation, []error{errors.New("idx 1: violations: val '898765432109', [len:11]")}),
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+			err := Validate(tc.in)
+			fmt.Println(err)
+			require.Equal(t, err, tc.expectedErr)
+		})
+	}
+}
+
+func TestValidateReturnsMultipleValidationErrs(t *testing.T) {
+	testCases := []struct {
+		name        string
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			name: "vld-err:multiple",
+			in: User{
+				PhysParams: UserPhysParams{Age: 17, Height: 300, Weight: 200},
+				ID:         "d56e42f5",
+				Name:       "John",
+				Experience: 1,
+				Email:      "any@@any.com",
+				Role:       "moder",
+				Phones:     []string{"891237456789", "898765432107"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "PhysParams",
+					Err: ValidationErrors{
+						ValidationError{
+							Field: "Age",
+							Err:   fmt.Errorf("%w: val '17', %v", errValidation, []string{"min:18"}),
+						},
+						ValidationError{
+							Field: "Height",
+							Err:   fmt.Errorf("%w: val '300', %v", errValidation, []string{"max:240"}),
+						},
+						ValidationError{
+							Field: "Weight",
+							Err:   fmt.Errorf("%w: val '200', %v", errValidation, []string{"max:150"}),
+						},
+					},
+				},
+				ValidationError{
+					Field: "ID",
+					Err:   fmt.Errorf("%w: val 'd56e42f5', %v", errValidation, []string{"len:36"}),
+				},
+				ValidationError{
+					Field: "Experience",
+					Err:   fmt.Errorf("%w: val '1', %v", errValidation, []string{"min:2"}),
+				},
+				ValidationError{
+					Field: "Email",
+					Err:   fmt.Errorf("%w: val 'any@@any.com', %v", errValidation, []string{"regexp:^\\w+@\\w+\\.\\w+$"}),
+				},
+				ValidationError{
+					Field: "Role",
+					Err:   fmt.Errorf("%w: val 'moder', %v", errValidation, []string{"in:admin,stuff"}),
+				},
+				ValidationError{
+					Field: "Phones",
+					Err: fmt.Errorf("%w: %v", errValidation, []error{
+						errors.New("idx 0: violations: val '891237456789', [len:11]"),
+						errors.New("idx 1: violations: val '898765432107', [len:11]"),
+					}),
+				},
+			},
 		},
-		// ...
-		// Place your code here.
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
 			t.Parallel()
-
-			// Place your code here.
-			_ = tt
+			err := Validate(tc.in)
+			fmt.Println(err)
+			require.Equal(t, err, tc.expectedErr)
 		})
 	}
 }
